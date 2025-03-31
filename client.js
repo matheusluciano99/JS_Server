95635ed47acccb205634184086327d1e4d6ccf85
matheusluciano99 <matheusluciano99@gmail.com>
2025-03-31 15:41:48 -0300

    token obtido

--- client.js
+++ client.js
@@ -2,3 +2,17 @@ const axios = require("axios");
 axios
   .get("https://catfact.ninja/fact")
   .then((response) => console.log(response.data.fact));
+
+// send a POST request
+axios({
+  method: "post",
+  url: "https://servidor-exercicios-js-eficaz.vercel.app/token",
+  data: { username: "matheuslaos" },
+}).then(
+  (response) => {
+    console.log(response);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
