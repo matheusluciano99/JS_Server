5698e1166ff40fa68091a175d52f3f5dbcba9057
matheusluciano99 <matheusluciano99@gmail.com>
2025-04-01 09:29:03 -0300

    exercicio 1 e 2 passando

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
-axios
-  .get("https://catfact.ninja/fact")
-  .then((response) => console.log(response.data.fact));
 
-// send a POST request
+// Declare o token em um escopo mais amplo
+let token;
+let exercicios;
+
 axios({
   method: "post",
   url: "https://servidor-exercicios-js-eficaz.vercel.app/token",
@@ -15,10 +15,10 @@ axios({
 })
   .then((response) => {
     // Extrair o token da resposta
-    const token = response.data.accessToken;
+    token = response.data.accessToken;
     console.log("Token recebido:", token);
 
-    // Usar o token na próxima requisição
+    // Usar o token para buscar os exercícios
     return axios.get(
       "https://servidor-exercicios-js-eficaz.vercel.app/exercicio",
       {
@@ -30,23 +30,50 @@ axios({
       }
     );
   })
-  .then(
-    (response) => {
-      console.log(response.data);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  .then((response) => {
+    // Salvar todos os exercícios
+    exercicios = response.data;
+    console.log("Exercícios recebidos:", exercicios);
 
-// // get request
-// axios
-//   .get("https://servidor-exercicios-js-eficaz.vercel.app/exercicio", {
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//       Authorization: "Bearer TOKEN",
-//     },
-//   })
-//   .then((response) => console.log(response.data))
-//   .catch((error) => console.log(error));
+    // Resolver todos os exercícios
+    const resultadoSoma = exercicios.soma.entrada.a + exercicios.soma.entrada.b;
+    const resultadoTamanho = exercicios["tamanho-string"].entrada.string.length;
+
+    console.log("Soma:", resultadoSoma);
+    console.log("Tamanho da string:", resultadoTamanho);
+
+    // Configuração de cabeçalhos comum
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+
+    // Enviar todos os exercícios em paralelo
+    return Promise.all([
+      axios.post(
+        "https://servidor-exercicios-js-eficaz.vercel.app/exercicio/soma",
+        { resposta: resultadoSoma },
+        { headers }
+      ),
+      axios.post(
+        "https://servidor-exercicios-js-eficaz.vercel.app/exercicio/tamanho-string",
+        { resposta: resultadoTamanho },
+        { headers }
+      ),
+    ]);
+  })
+  .then((responses) => {
+    // responses é um array com as respostas de cada exercício
+    console.log(responses[0].data);
+    console.log(responses[1].data);
+
+    // Se quiser fazer mais alguma coisa com as respostas, pode fazer aqui
+    return responses;
+  })
+  .catch((error) => {
+    console.log("Erro:", error.message);
+    if (error.response) {
+      console.log("Detalhes do erro:", error.response.data);
+    }
+  });
